3ba1fbdefb5db9fac1c71e3aa3a54258d38b3520
RobertJonez <robertdjonez@gmail.com>
2021-01-27 13:46:36 -0700

    Fixed it

--- scripts/Game.js
+++ scripts/Game.js
@@ -24,11 +24,11 @@ class Game {
         this.context = this.canvas.getContext('2d');
         alert("You have 10 seconds to callibrate your device before the game begins. To callibrate, move your cursor to the four corners of your screen and the center of your screen and click multiple times while looking at your cursor. Failure to do this may result in harder to control character. Try not to move your head, your cursor will move with your eyes. ");
         alert("Avoid objects and try to collect tokens! You have 10 seconds before the game starts!");
-        setTimeout(() => {
+       // setTimeout(() => {
             this.populateScreen();
             this.counter();
             window.requestAnimationFrame(timeStamp => {this.gameLoop(timeStamp)});
-        }, 10000)
+       // }, 10000)
     }
 
     /*********************************************************************************
